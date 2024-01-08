ccc13b8813a9c886537d300c41bd27324469474a
Anton Savytskyi <antonsav123@gmail.com>
2024-01-08 03:10:59 +0100

    add ABC programm

--- lab3/ABC.cpp
+++ lab3/ABC.cpp
@@ -3,11 +3,19 @@
 using namespace std;
 
 int main() {
-    int A;
-    int B;
-    int C;
-    cin >> A;
-    cin >> B;
-    cin >> C;
+    int A, B, C;
+    cin >> A >> B >> C;
+    cout << "A = " << A << " | B = " << B << " | C = " << C << "\n";
+
+    if (A > B) {
+        swap(A, B);
+    }
+    if (B > C) {
+        swap(B, C);
+    }
+    if (A > B) {
+        swap(A, B);
+    }
+
     cout << "A = " << A << " | B = " << B << " | C = " << C << "\n";
 }
